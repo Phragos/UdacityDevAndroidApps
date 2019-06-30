74443e4c1faa8f672d78ad6a6952b4086e4fcf7b
Phragos <dfernandez01@uoc.edu>
2019-06-30 09:27:38 +0200

    Implemented binding and added an input method manager

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.java
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.aboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.inputmethod.InputMethodManager;
+//import android.widget.EditText;
+//import android.widget.TextView;
+
+import com.example.aboutme.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // This is no longer needed when using binding
+        //setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        findViewById(R.id.done_button).setOnClickListener(new View.OnClickListener() {
+        // This is without layout binding
+        /*findViewById(R.id.done_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNickname(view);
+            }
+        });*/
+
+        // This is with layout binding
+        binding.doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addNickname(view);
@@ -23,12 +41,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addNickname(View view) {
-        EditText nicknameEditText = findViewById(R.id.nickname_edit);
+        // This is needed if no layout binding exists
+        /*EditText nicknameEditText = findViewById(R.id.nickname_edit);
         TextView nicknameTextView = findViewById(R.id.nickname_text);
 
         nicknameTextView.setText(nicknameEditText.getText().toString());
         nicknameEditText.setVisibility(View.GONE);
         view.setVisibility(View.GONE);
-        nicknameTextView.setVisibility(View.VISIBLE);
+        nicknameTextView.setVisibility(View.VISIBLE);*/
+
+        // With layout binding
+        binding.nicknameText.setText(binding.nicknameEdit.getText());
+        binding.nicknameEdit.setVisibility(View.GONE);
+        binding.doneButton.setVisibility(View.GONE);
+        binding.nicknameText.setVisibility(View.VISIBLE);
+
+        // This is to make sure the keyboard gets hidden
+         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
