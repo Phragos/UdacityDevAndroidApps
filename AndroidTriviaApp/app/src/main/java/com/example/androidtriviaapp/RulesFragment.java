4421f7ba398e02bfd7eaf2dac93e013b55ee3fa3
Phragos <dfernandez01@uoc.edu>
2019-07-07 21:24:51 +0200

    Init Trivia app with Java Resources have been moved. Working on Fragments info now

---
+++ AndroidTriviaApp/app/src/main/java/com/example/androidtriviaapp/RulesFragment.java
@@ -0,0 +1,30 @@
+package com.example.androidtriviaapp;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RulesFragment extends Fragment {
+
+
+    public RulesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rules, container, false);
+    }
+
+}
