d776ecd656c9149ffaad739fed6e35dcb2ebfdd1
Phragos <dfernandez01@uoc.edu>
2019-07-07 20:53:44 +0200

    Init Trivia app with Java

---
+++ AndroidTriviaApp/app/src/main/java/com/example/androidtriviaapp/TitleFragment.java
@@ -0,0 +1,33 @@
+package com.example.androidtriviaapp;
+
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TitleFragment extends Fragment {
+
+
+    public TitleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewDataBinding FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false);
+        return FragmentTitleBinding.getRoot();
+    }
+
+}
